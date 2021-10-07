59b392edd3cee8806034fd52b4d04ce92fae3aa8
Mohammad Yasir <mdcode2021@gmail.com>
2021-10-07 21:47:39 +0530

    Wrapped Calendar in TabView

--- XCalendar/Files/App/XCalendar.swift
+++ XCalendar/Files/App/XCalendar.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct XCalendar: View {
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
+        TabView {
             ForEach(Key.calendar.getAllMonths(forDate: Date()), id:\.self) { day in
                 CalendarView(forDate: day, selectionSize: .large)
                     .padding(.vertical)
             }
         }
+        .tabViewStyle(.page(indexDisplayMode: .never))
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).edgesIgnoringSafeArea(.all))
     }
